d5c877e7a5a51ee219f89b221e074f04a0850e12
Jayamounika26 <154662990+Jayamounika26@users.noreply.github.com>
2026-07-20 11:51:55 +0530

    ✅ Add Two Integers | Easy | Python | Runtime: 0 ms (100.00%) | Memory: 12.1 MB | 2026-07-20

---
+++ leetcode/easy/2235-add-two-integers/solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def sum(self, num1, num2):
+        sum=num1+num2
+        return sum
+        
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        
